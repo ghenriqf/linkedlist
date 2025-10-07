d4e0d845c5573393213dad27f96e098772aa45d2
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2025-10-07 15:16:24 -0300

    implementa metodo de insert

--- main.cpp
+++ main.cpp
@@ -4,37 +4,30 @@ using namespace std;
 struct Node {
     int data;
     Node *next;
-};
 
+    Node (int data){
+        data = data;
+    }
+};
 
 struct linkedList {
 
     Node* head;
     
-    Node* iniciarList(int data) {
-        head -> data = data;
+    void linkedlist() {
         head -> next = nullptr;
-
-        return head;
     }
 
-    void inserir(Node *node, int data) {
+    void insert(Node*& head,int data) {
 
         if (head -> next == nullptr) {
-            head -> next = node;
-            return;
+            head -> next = new Node(data);
+            return; 
         }
-        return inserir(head -> next, int data);
+        insert(head -> next,data);
     }
 
 };
 
-
-
 int main () {
-    int x = 10;
-    linkedList list;
-    list.head = list.iniciarList(x);
-
-    cout << list.head -> next;
 }
