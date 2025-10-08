2df2ef7e04c4b2b8a54f726feb3198bedcb7072e
Gabriel Henrique <160789094+ghenriqf@users.noreply.github.com>
2025-10-07 21:16:05 -0300

    finalizar projeto

--- main.cpp
+++ main.cpp
@@ -3,31 +3,112 @@ using namespace std;
 
 struct Node {
     int data;
-    Node *next;
+    Node* next;
 
-    Node (int data){
-        data = data;
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
     }
 };
 
-struct linkedList {
-
+struct ListaInteiro {
     Node* head;
-    
-    void linkedlist() {
-        head -> next = nullptr;
+    int tamanho;
+
+    ListaInteiro() {
+        head = nullptr;
+        tamanho = 0;
     }
 
-    void insert(Node*& head,int data) {
-
-        if (head -> next == nullptr) {
-            head -> next = new Node(data);
-            return; 
+    void inserirFinal(int valor, Node*& atual) {
+        if (atual == nullptr) {
+            atual = new Node(valor);
+            tamanho++;
+            return;
         }
-        insert(head -> next,data);
+        inserirFinal(valor, atual->next);
     }
 
+    void inserirPosicao(int valor, int pos, Node*& atual) {
+        if (pos < 0 || pos > tamanho) {
+            cout << "Posicao invalida!\n";
+            return;
+        }
+        if (pos == 0) {
+            Node* novo = new Node(valor);
+            novo->next = atual;
+            atual = novo;
+            tamanho++;
+            return;
+        }
+        inserirPosicao(valor, pos - 1, atual->next);
+    }
+
+    void removerPosicao(int pos, Node*& atual) {
+        if (pos < 0 || pos >= tamanho) {
+            cout << "Posicao invalida!\n";
+            return;
+        }
+        if (pos == 0) {
+            Node* temp = atual;
+            atual = atual->next;
+            delete temp;
+            tamanho--;
+            return;
+        }
+        removerPosicao(pos - 1, atual->next);
+    }
+
+    void exibir(Node* atual) {
+        if (atual == nullptr) return;
+        cout << atual->data << " ";
+        exibir(atual->next);
+    }
+
+    void exibirInverso(Node* atual) {
+        if (atual == nullptr) return;
+        exibirInverso(atual->next);
+        cout << atual->data << " ";
+    }
+
+    void limpar(Node* atual) {
+        if (atual == nullptr) return;
+        limpar(atual->next);
+        delete atual;
+    }
 };
 
-int main () {
+int main() {
+    ListaInteiro lista;
+
+    cout << "Inserindo 10, 20, 30 no final:" << endl;
+    lista.inserirFinal(10, lista.head);
+    lista.inserirFinal(20, lista.head);
+    lista.inserirFinal(30, lista.head);
+    lista.exibir(lista.head);
+    cout << endl;
+
+    cout << "Inserindo 50 na posicao 1:" << endl;
+    lista.inserirPosicao(50, 1, lista.head);
+    lista.exibir(lista.head);
+    cout << endl;
+
+    cout << "Removendo elemento da posicao 2:" << endl;
+    lista.removerPosicao(2, lista.head);
+    lista.exibir(lista.head);
+    cout << endl;
+
+    cout << "Exibindo em ordem inversa:" << endl;
+    lista.exibirInverso(lista.head);
+    cout << endl;
+
+    lista.limpar(lista.head);
+    lista.head = nullptr;
+    lista.tamanho = 0;
+
+    cout << "Lista apos liberar memoria: ";
+    lista.exibir(lista.head);
+    cout << endl;
+
+    return 0;
 }
